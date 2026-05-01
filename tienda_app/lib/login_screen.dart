28adf0e047b88e8aa546e1c85cff23bc1af089a6
John Barros <johnbarrosrivero@gmail.com>
2026-05-01 16:45:35 -0500

    shared preferences

--- tienda_app/lib/login_screen.dart
+++ tienda_app/lib/login_screen.dart
@@ -1,5 +1,6 @@
-import 'home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
